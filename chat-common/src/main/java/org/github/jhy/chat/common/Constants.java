3d0272ca4756ce74088bde5a9b087d716d615ab3
JiHongYuan <1206429202@qq.com>
2023-02-01 14:50:21 +0800

    feat[common]: 错误码

---
+++ chat-common/src/main/java/org/github/jhy/chat/common/Constants.java
@@ -0,0 +1,27 @@
+package org.github.jhy.chat.common;
+
+/**
+ * @author jihongyuan
+ * @date 2023/2/1 11:24
+ */
+public class Constants {
+
+    /** 成功 */
+    public static final String SUCCESS = "0";
+
+    /** 服务器内部错误 */
+    public static final String ERROR = "-1";
+
+    /** 注册错误 */
+    public static final String SIGN_UP_ERROR = "1000";
+
+    /** 注册用户名存在 */
+    public static final String SIGN_UP_NAME_EXISTS = "1001";
+
+    /** 登录错误 */
+    public static final String SIGN_IN_ERROR = "2000";
+
+    /** 登录用户名密码错误 */
+    public static final String SIGN_IN_NAME_OR_PASSWORD_ERROR = "2001";
+
+}
